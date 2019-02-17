385078933cce4c6cdab5174096bbbdab18caddca
avocadokek <47501889+avocadokek@users.noreply.github.com>
2019-02-17 18:15:15 +0200

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+
+    class Employee
+    {
+        
+        private int _age;
+        string _surname;
+
+        public Employee(int _age, string _surname)
+        {
+            
+                this._age = 35;
+                this._surname = "Rita";
+            
+            throw new Exception();
+        }
+
+        public Employee()
+        {
+            _age = 27;
+            _surname = "Ivan";
+            throw new Exception();
+        }
+        private string GetEmployeeAge(int Age)
+        {
+            _age = Age;
+            return Convert.ToString(_age);
+            throw new Exception();
+        }
+
+        public string GetInfo()
+        {
+
+            _surname = "Name";
+            _age = 28;
+            return $"{_age} + {_surname}";
+            throw new Exception();
+        }
+
+        public void SetSurname(string value)
+        {
+            _surname = value;
+            throw new Exception();
+        } 
+    
+    
+}
+
+}
